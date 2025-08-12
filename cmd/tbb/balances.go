6a4c9aa0a5ce153a1782cdbfabc5b54c79b57359
jh.cheng <jhcheng.alan@gmail.com>
2025-08-12 22:07:23 +0800

    init: create main.go version.go balances.go

---
+++ cmd/tbb/balances.go
@@ -0,0 +1,33 @@
+func balancesCmd() *cobra.Command {
+	var balancesCmd = &cobra.Command{
+		Use:   "balances",
+		Short: "Interact with balances (list...)",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return incorrectUsageErr()
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+	balancesCmd.AddCommand(balancesListCmd)
+	return balancesCmd
+}
+
+var balancesListCmd = &cobra.Command {
+	Use: "list",
+	Short: "Lists all balances",
+	Run: func(cmd *cobra.Command, args []string) {
+		state, err := database.NewStateFromDisk()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer state.Close()
+
+		fmt.Pirntln("Account balances:")
+		fmt.Println("--------------------------------")
+		fmt.println("")
+		for account, balance := range state.Balances() {
+			fmt.Println(fmt.Sprintf("%s: %s", account, balance))
+		}
+	}
+}
